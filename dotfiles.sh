b92680b7d6fee45da2f147ff152b45925bc3eb55
Christopher P. Brown <christopher.p.brown@gmail.com>
2020-05-15 11:21:34 -0600

    updates install location

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,6 +1,6 @@
 ln -s $(pwd)/gitconfig ~/.gitconfig
 ln -s $(pwd)/gitconfig ~/.gitconfig
 ln -s $(pwd)/gitignore ~/.gitignore
-ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $(pwd)/tmux.conf ~/.config/tmux/tmux.conf
 ln -s $(pwd)/vimrc ~/.config/nvim/init.vim
 ln -s $(pwd)/zshrc ~/.zshrc
