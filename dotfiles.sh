92dab3b07e5203ce712a910fa8d9738424c631cc
Christopher P. Brown <christopher.p.brown@gmail.com>
2020-05-15 10:40:53 -0600

    add script. remove gpg signing for now

---
+++ dotfiles.sh
@@ -0,0 +1,6 @@
+ln -s $(pwd)/gitconfig ~/.gitconfig
+ln -s $(pwd)/gitconfig ~/.gitconfig
+ln -s $(pwd)/gitignore ~/.gitignore
+ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $(pwd)/vimrc ~/.config/nvim/init.vim
+ln -s $(pwd)/zshrc ~/.zshrc
