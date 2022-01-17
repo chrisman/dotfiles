ff7751b13c15ea5bce58d572252ac2a16b310f45
Christopher P. Brown <christopher.p.brown@gmail.com>
2022-01-17 10:18:39 -0700

    add some brews

--- install_brews.sh
+++ install_brews.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-brew install rectangle docker keepassxc firefox caffeine slack postico kap --cask
-brew install nvm tmux mosh lynx neovim pandoc
+brew install rectangle keepassxc firefox caffeine slack --cask
+brew install nvm tmux mosh lynx neovim pandoc ranger urlview diff-so-fancy fzf the_silver_searcher
