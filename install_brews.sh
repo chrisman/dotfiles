1d8a9caf3ecfdfb5e9d8323b4d073943cb2f8ddc
Christopher P. Brown <christopher.p.brown@gmail.com>
2022-08-12 10:23:28 -0400

    zsh

--- install_brews.sh
+++ install_brews.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-brew install rectangle keepassxc firefox caffeine slack --cask
-brew install nvm tmux mosh lynx neovim pandoc ranger urlview diff-so-fancy fzf the_silver_searcher
+brew install rectangle keepassxc firefox caffeine slack drawio macdown postman --cask
+brew install nvm tmux mosh lynx neovim pandoc ranger urlview diff-so-fancy fzf the_silver_searcher gdate
